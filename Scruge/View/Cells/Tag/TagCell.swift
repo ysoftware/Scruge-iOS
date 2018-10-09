7d2607daf08e021fd1a53115e611b8ae6e573438
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-10-09 12:57:06 +0300

    Tags in search view

---
+++ Scruge/View/Cells/Tag/TagCell.swift
@@ -0,0 +1,27 @@
+//
+//  TagCell.swift
+//  Scruge
+//
+//  Created by ysoftware on 09/10/2018.
+//  Copyright © 2018 Ysoftware. All rights reserved.
+//
+
+import UIKit
+
+final class TagCell: UICollectionViewCell {
+
+	@IBOutlet weak var backView: RoundedView!
+	@IBOutlet weak var tagLabel:UILabel!
+
+	@discardableResult
+	func setup(with tag:String) -> Self {
+		tagLabel.text = "#\(tag)"
+		return self
+	}
+
+	@discardableResult
+	func setSelected(_ value:Bool) -> Self {
+		backView.backgroundColor = value ? .black : .gray
+		return self
+	}
+}
--- Scruge/View/Controllers/Campaign/SearchVC.swift
+++ Scruge/View/Controllers/Campaign/SearchVC.swift
@@ -13,11 +13,14 @@ final class SearchViewController: UIViewController {
 	// MARK: - Outlets
 
 	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var tagCollectionView: UICollectionView!
 
 	// MARK: - Properties
 
 	private let searchController = UISearchController(searchResultsController: nil)
 	private let vm = CampaignAVM()
+	private let tags = ["crypto", "services", "marketplace", "ico", "technology"]
+	private var selectedTags = [""]
 
 	// MARK: - Setup
 
@@ -25,6 +28,29 @@ final class SearchViewController: UIViewController {
 		super.viewDidLoad()
 
 		setupSearchBar()
+		setupCollection()
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		// The following block also "fixes" the problem without jumpiness after the view has already appeared on screen.
+		DispatchQueue.main.async {
+			self.tagCollectionView.collectionViewLayout.invalidateLayout()
+		}
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		// The following line makes cells size properly in iOS 12.
+		tagCollectionView.collectionViewLayout.invalidateLayout()
+	}
+
+	private func setupCollection() {
+		tagCollectionView.register(UINib(resource: R.nib.tagCell),
+								   forCellWithReuseIdentifier: R.reuseIdentifier.tagCell.identifier)
+		let layout = tagCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
+		layout?.itemSize = UICollectionViewFlowLayout.automaticSize
+		layout?.estimatedItemSize = CGSize(width: 100, height: 50)
 	}
 
 	private func setupSearchBar() {
@@ -33,7 +59,7 @@ final class SearchViewController: UIViewController {
 		searchController.searchBar.delegate = self
 
 		searchController.hidesNavigationBarDuringPresentation = false
-		searchController.dimsBackgroundDuringPresentation = true
+		searchController.dimsBackgroundDuringPresentation = false
 
 		navigationItem.titleView = searchController.searchBar
 		definesPresentationContext = true
@@ -59,5 +85,43 @@ extension SearchViewController: UITableViewDataSource {
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		return UITableViewCell()
+	}
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+
+	func collectionView(_ collectionView: UICollectionView,
+						numberOfItemsInSection section: Int) -> Int {
+		return tags.count
+	}
+
+	func collectionView(_ collectionView: UICollectionView,
+						cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let tag = tags[indexPath.item]
+		return collectionView.dequeueReusableCell(
+			withReuseIdentifier: R.reuseIdentifier.tagCell, for: indexPath)!
+			.setup(with: tag)
+			.setSelected(selectedTags.contains(tag))
+	}
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+
+	func collectionView(_ collectionView: UICollectionView,
+						didSelectItemAt indexPath: IndexPath) {
+		let cell = collectionView.cellForItem(at: indexPath) as! TagCell
+		let tag = tags[indexPath.item]
+
+		if selectedTags.contains(tag) {
+			cell.setSelected(false)
+			selectedTags.removeAll(where: { $0 == tag })
+		}
+		else {
+			cell.setSelected(true)
+			selectedTags.append(tag)
+		}
+
+		vm.query?.tags = selectedTags
+		vm.reloadData()
 	}
 }
