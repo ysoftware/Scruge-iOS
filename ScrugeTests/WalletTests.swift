94e3fabac89916539ccf8514f9e45f34efe46916
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-10-20 21:35:44 +0300

    Wallet unlock testing

---
+++ ScrugeTests/WalletTests.swift
@@ -0,0 +1,86 @@
+//
+//  ScrugeTests.swift
+//  ScrugeTests
+//
+//  Created by ysoftware on 24.09.2018.
+//  Copyright © 2018 Ysoftware. All rights reserved.
+//
+
+import XCTest
+@testable import Scruge
+
+let PASSCODE = "123456"
+let PRIVATE_KEY = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+
+final class WalletTests: XCTestCase {
+
+	// MARK: - Import
+
+	func importKeyBlock(_ expectation:XCTestExpectation) -> (SELocalAccount?)->Void {
+		return { account in
+			if account != nil {
+				expectation.fulfill()
+			}
+		}
+	}
+
+	func testSuccessImport() {
+		let expectation = XCTestExpectation()
+		expectation.expectationDescription = "this key should be imported correctly"
+		// correct private key created by keosd
+		Service.wallet.importKey(PRIVATE_KEY,
+								 passcode: PASSCODE, importKeyBlock(expectation))
+		wait(for: [expectation], timeout: 0.1)
+	}
+
+	func testFailImport() {
+		let expectation = XCTestExpectation()
+		expectation.expectationDescription = "these keys should fail import"
+		expectation.isInverted = true
+
+		// public key instead of private key
+		Service.wallet.importKey("EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
+								 passcode: PASSCODE, importKeyBlock(expectation))
+		// modified private key
+		Service.wallet.importKey("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD2",
+								 passcode: PASSCODE, importKeyBlock(expectation))
+		// some random string
+		Service.wallet.importKey("fail",
+								 passcode: PASSCODE, importKeyBlock(expectation))
+		wait(for: [expectation], timeout: 0.3)
+	}
+
+	// MARK: - Lock
+
+	func testUnlockSuccess() {
+		let expectation = XCTestExpectation()
+		expectation.expectationDescription = "account should unlock"
+		Service.wallet.importKey(PRIVATE_KEY,
+								 passcode: PASSCODE) { account in
+									guard let account = account else {
+										return XCTFail("this key should be imported correctly")
+									}
+									try? account.timedUnlock(passcode: PASSCODE, timeout: 0.3)
+									if !account.isLocked() {
+										expectation.fulfill()
+									}
+		}
+		wait(for: [expectation], timeout: 0.2)
+	}
+
+	func testUnlockFailure() {
+		let expectation = XCTestExpectation()
+		expectation.expectationDescription = "account should not unlock with an incorrect passcode"
+		Service.wallet.importKey(PRIVATE_KEY,
+								 passcode: PASSCODE) { account in
+									guard let account = account else {
+										return XCTFail("this key should be imported correctly")
+									}
+									try? account.timedUnlock(passcode: "098765", timeout: 0.3)
+									if account.isLocked() {
+										expectation.fulfill()
+									}
+		}
+		wait(for: [expectation], timeout: 0.2)
+	}
+}
