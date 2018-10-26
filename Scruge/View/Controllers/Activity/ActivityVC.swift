381b818148ea2f434bd1f1691f9eb962769e78c9
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-10-26 12:08:03 +0300

    ActivityVC

---
+++ Scruge/View/Controllers/Activity/ActivityVC.swift
@@ -0,0 +1,95 @@
+//
+//  ActivityVC.swift
+//  Scruge
+//
+//  Created by ysoftware on 03/10/2018.
+//  Copyright © 2018 Ysoftware. All rights reserved.
+//
+
+import UIKit
+import MVVM
+
+final class ActivityViewController: UIViewController {
+
+	// MARK: - Outlets
+
+	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var loadingView: LoadingView!
+	
+	// MARK: - Properties
+
+	private let vm = UpdateAVM(.activity)
+	private var tableUpdateHandler:ArrayViewModelUpdateHandler!
+
+	// MARK: - Setup
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupVM()
+		setupTableView()
+	}
+
+	private func setupTableView() {
+		tableView.estimatedRowHeight = 100
+		tableView.rowHeight = UITableView.automaticDimension
+		tableView.register(UINib(resource: R.nib.updateCell),
+						   forCellReuseIdentifier: R.reuseIdentifier.updateCell.identifier)
+	}
+
+	private func setupVM() {
+		tableUpdateHandler = ArrayViewModelUpdateHandler(with: tableView)
+		vm.delegate = self
+		vm.reloadData()
+	}
+}
+
+extension ActivityViewController: UITableViewDataSource {
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return vm.numberOfItems
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.updateCell,
+											 for: indexPath)!
+			.setup(with: vm.item(at: indexPath.row, shouldLoadMore: true))
+	}
+}
+
+extension ActivityViewController: UITableViewDelegate {
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
+}
+
+extension ActivityViewController: ArrayViewModelDelegate {
+
+	func didUpdateData<M, VM, Q>(_ arrayViewModel: ArrayViewModel<M, VM, Q>,
+								 _ update: MVVM.Update)
+		where M : Equatable, VM : ViewModel<M>, Q : Query {
+			tableUpdateHandler.handle(update)
+	}
+
+	func didChangeState<M, VM, Q>(_ arrayViewModel: ArrayViewModel<M, VM, Q>,
+								  to state: ArrayViewModelState)
+		where M : Equatable, VM : ViewModel<M>, Q : Query {
+
+			switch vm.state {
+			case .error(let error):
+				let message = ErrorHandler.message(for: error)
+				loadingView.set(state: .error(message))
+			case .loading, .initial:
+				loadingView.set(state: .loading)
+			case .ready:
+				if vm.isEmpty {
+					loadingView.set(state: .error("No updates"))
+				}
+				else {
+					loadingView.set(state: .ready)
+				}
+			default: break
+			}
+	}
+}
