f30e7898635d95112b4074879c0cf76c7bc8c09a
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-12-28 11:51:32 +0300

    removed debug prints

--- Scruge/Services/EOS/EOS.swift
+++ Scruge/Services/EOS/EOS.swift
@@ -38,7 +38,6 @@ struct EOS {
 	func getActions(for account:String,
 					query:ActionsQuery?,
 					completion: @escaping (Result<[ActionReceipt], AnyError>)->Void) {
-		print("\(query!.position), \(query!.offset)")
 		chain.getActions(accountName: account,
 						 position: query?.position ?? -1,
 						 offset: query?.offset ?? -100) { result, error in
@@ -51,8 +50,6 @@ struct EOS {
 			if !actions.isEmpty, query?.position == -1 {
 				query?.set(limit: actions[0].accountActionSeq)
 			}
-
-			print("loaded: \(actions.count)")
 			completion(.success(actions))
 		}
 	}
